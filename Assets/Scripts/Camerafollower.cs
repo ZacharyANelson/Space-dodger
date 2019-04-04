40823e7b3e4ef1183cf1a5921832a147a2cefb9e
ZacharyANelson <47261023+ZacharyANelson@users.noreply.github.com>
2019-04-03 21:50:27 -0500

    Initial commit

---
+++ Assets/Scripts/Camerafollower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camerafollower : MonoBehaviour
+{
+    // The Target
+    public Transform target;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x,
+                                         transform.position.y,
+                                         transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour
+{
+ 
+    public float speed = 2f;
+
+
+    public float force = 300f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+        if (Input.GetKeyDown(KeyCode.D))
+            speed = 10f;
+        else
+            speed = 2f;
+    }
+}
